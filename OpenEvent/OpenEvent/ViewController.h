2be295a1356af0656689a9b349b00b875981b298
Meiwin <meiwin@pie.co>
2016-02-02 17:06:35 +0800

    initial commit

---
+++ OpenEvent/OpenEvent/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenEvent
+//
+//  Created by Meiwin Fu on 2/2/16.
+//  Copyright © 2016 FOSSASIA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OpenEvent/OpenEvent/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OpenEvent
+//
+//  Created by Meiwin Fu on 2/2/16.
+//  Copyright © 2016 FOSSASIA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
